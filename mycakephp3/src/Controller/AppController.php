9327ea1660ce44bd400911c6ed06c4a5cf4d0da7
zhangjun <5024064@qq.com>
2017-10-31 15:15:39 +0800

    加载Auth组件，测试时注视掉了

--- mycakephp3/src/Controller/AppController.php
+++ mycakephp3/src/Controller/AppController.php
@@ -55,26 +55,25 @@ class AppController extends Controller
         //$this->loadComponent('Security');
         //$this->loadComponent('Csrf');
 
-//        $this->loadComponent('Auth', [
-//            'authorize' => [
-//                'controller'
-//            ],
-//            'authenticate' => [
-//                'Form' => [
-//                    'finder' => 'auth'
-//                ]
-//            ],
-//            'loginRedirect' => [
-//                'controller' => 'Users',
-//                'action' => 'index'
-//            ],
-//            'logoutRedirect' => [
-//                'controller' => 'Users',
-//                'action' => 'login',
-//                'home'
-//            ]
-//        ]);
-
+        $this->loadComponent('Auth', [
+            'authorize' => [
+                'controller'
+            ],
+            'authenticate' => [
+                'Form' => [
+                    'finder' => 'auth'
+                ]
+            ],
+            'loginRedirect' => [
+                'controller' => 'Users',
+                'action' => 'index'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Users',
+                'action' => 'login',
+                'home'
+            ]
+        ]);
     }
 
     /**
